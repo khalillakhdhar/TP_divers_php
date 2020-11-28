dacb063de226b75fc23ca8ef3660bbc8478a8308
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-28 17:24:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,28 @@
 
 </head>
 
+<body>
+    <form>
+        <table>
+            <tr>
+                <td>Nom:</td>
+                <td><input type="text" name="nom"></td>
+            </tr>
+            <tr>
+                <td>Prenom:</td>
+                <td><input type="text" name="prenom"></td>
+            </tr>
+            <tr>
+                <td>Salaire Brute</td>
+                <td><input type="text" name="salaire"></td>
+            </tr>
+        </table>
+
+
+
+    </form>
+
+
+</body>
+
 </html>
